55a2f469461cb5668584c70a9360fc2bfbe2a7e5
Nikolay Yurin <nyurin@griddynamics.com>
2014-06-30 13:02:22 +0400

    [DMG-319] Add JIRA recipe, include local database setup

--- cookbooks/cicd_infrastructure/metadata.rb
+++ cookbooks/cicd_infrastructure/metadata.rb
@@ -6,3 +6,5 @@ license          ''
 description      'Installs/Configures cicd-infrastructure'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
+
+depends 'jira'
