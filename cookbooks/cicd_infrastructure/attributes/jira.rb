382cff439e2ed9427f3b9737d1b5c0e0629c1035
Nikolay Yurin <nyurin@griddynamics.com>
2014-07-03 16:45:18 +0400

    [DMG-319] Increase maximum jvm memory up to 1536m

--- cookbooks/cicd_infrastructure/attributes/jira.rb
+++ cookbooks/cicd_infrastructure/attributes/jira.rb
@@ -7,7 +7,7 @@
 #
 
 node.set['jira']['jvm']['minimum_memory']  = '768m'
-node.set['jira']['jvm']['maximum_memory']  = '1024m'
+node.set['jira']['jvm']['maximum_memory']  = '1536m'
 node.set['jira']['jvm']['maximum_permgen'] = '768m'
 node.set['jira']['jvm']['java_opts'] = '-Datlassian.plugins.enable.wait=300'
 
