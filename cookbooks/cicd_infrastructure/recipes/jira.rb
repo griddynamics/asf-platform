7aa990f3e14f60c3267538be1aa54ccaa97db7d7
Nikolay Yurin <nyurin@griddynamics.com>
2014-07-03 16:43:25 +0400

    [DMG-319] Temporary disable JIRA waiting script

--- cookbooks/cicd_infrastructure/recipes/jira.rb
+++ cookbooks/cicd_infrastructure/recipes/jira.rb
@@ -9,27 +9,25 @@
 include_recipe 'java::default'
 include_recipe 'jira::default'
 
-ruby_block "wait for JIRA" do
-    block do
-        require "net/https"
-        require "uri"
+# ruby_block "wait for JIRA" do
+#     block do
+#         require "net/https"
+#         require "uri"
 
-        host = node['jira']['apache2']['virtual_host_name']
-        port = node['jira']['apache2']['ssl']['port']
-        path = "secure/SetupApplicationProperties!default.jspa"
+#         host = node['jira']['apache2']['virtual_host_name']
+#         port = node['jira']['apache2']['ssl']['port']
+#         path = "secure/SetupApplicationProperties!default.jspa"
 
-        uri = URI.parse("https://#{host}:#{port}/#{path}")
-        http = Net::HTTP.new(uri.host, uri.port)
-        http.use_ssl = true
-        http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+#         uri = URI.parse("https://#{host}:#{port}/#{path}")
+#         http = Net::HTTP.new(uri.host, uri.port)
+#         http.use_ssl = true
+#         http.verify_mode = OpenSSL::SSL::VERIFY_NONE
 
-        request = Net::HTTP::Get.new(uri.request_uri)
+#         request = Net::HTTP::Get.new(uri.request_uri)
 
-        10.times {
-            |x| Chef::Log.info("Trying to connect to https://#{host}:#{port}... #{10 - x} retries remain")
-            break if http.request(request).code.eql?('200')
-            sleep(30)
-        }
-    end
-end
-
+#         10.times {
+#             sleep(60)
+#             break if File.read("/opt/atlassian/jira/logs/catalina.out").include?("You can now access JIRA through your web browser")
+#         }
+#     end
+# end
