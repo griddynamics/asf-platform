c045ba75d7059f15e975640c30756c9f04293f6c
Aleksei Kornev <akornev@griddynamics.com>
2014-07-07 14:29:56 +0400

    [DMG-320] Add ability to deploy jenkins

---
+++ cookbooks/cicd_infrastructure/recipes/jenkins.rb
@@ -0,0 +1,11 @@
+# Encoding: utf-8
+#
+# Cookbook Name:: cicd_infrastructure
+# Recipe:: jenkins
+#
+# Copyright 2014, Grid Dynamics International, Inc.
+#
+
+include_recipe 'java::default'
+include_recipe 'jenkins::master'
+
