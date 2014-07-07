fe859e32ab6fbf751b4059c7581784d045036046
Nikolay Yurin <nyurin@griddynamics.com>
2014-07-07 19:08:00 +0400

    [DMG-323] Temporary use plaintext password

--- cookbooks/cicd_infrastructure/attributes/openldap.rb
+++ cookbooks/cicd_infrastructure/attributes/openldap.rb
@@ -8,7 +8,7 @@
 
 node.set['openldap']['basedn'] = 'dc=example,dc=com'
 node.set['openldap']['manage_ssl'] = false
-node.set['openldap']['rootpw'] = '{SSHA}d2BamRTgBuhC6SxC0vFGWol31ki8iq5m'
+node.set['openldap']['rootpw'] = 'password'
 
 default['openldap']['server'] = 'localhost'
 default['openldap']['users'] = [
