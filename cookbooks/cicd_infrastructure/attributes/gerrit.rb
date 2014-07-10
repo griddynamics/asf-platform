1d2b8ad32be18b22247470684ee30784b858b694
Grigory Silantiev <gsilantyev@griddynamics.com>
2014-07-10 12:09:02 +0400

    [DMG-325] Add headers for gerrit's and sonar's attribute files

--- cookbooks/cicd_infrastructure/attributes/gerrit.rb
+++ cookbooks/cicd_infrastructure/attributes/gerrit.rb
@@ -1,3 +1,11 @@
+# Encoding: utf-8
+#
+# Cookbook Name:: cicd-infrastructure
+# Attributes:: gerrit
+#
+# Copyright 2014, Grid Dynamics International, Inc.
+#
+
 default['gerrit']['version'] = "2.8.6"
 default['gerrit']['auth']['registerEmailPrivateKey'] = 'gerrit'
 default['gerrit']['auth']['restTokenPrivateKey'] = 'gerrit'
