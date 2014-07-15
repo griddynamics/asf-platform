d24d1890dddbcdb34327f3c5b4df18b3bd1a40be
Grigory Silantiev <gsilantyev@griddynamics.com>
2014-07-15 19:52:38 +0400

    [DMG-322] Automatization deployment of Zephyr

---
+++ cookbooks/cicd_infrastructure/recipes/zephyr.rb
@@ -0,0 +1,9 @@
+# Encoding: utf-8
+#
+# Cookbook Name:: cicd_infrastructure
+# Recipe:: chef-zephyr
+#
+# Copyright 2014, Grid Dynamics International, Inc.
+#
+
+include_recipe 'chef-zephyr::default'
