8eca5d675d5b0c1a5cd372dbeb26eca7876bf855
Grigory Silantiev <gsilantyev@griddynamics.com>
2014-07-17 18:23:29 +0400

    [DMG-351] Integration OpenLDAP with Sonar

--- cookbooks/cicd_infrastructure/recipes/sonar.rb
+++ cookbooks/cicd_infrastructure/recipes/sonar.rb
@@ -9,3 +9,23 @@
 include_recipe 'java'
 include_recipe 'sonar'
 include_recipe 'sonar::database_mysql'
+
+#install LDAP plugin
+
+sonar_plugin 'sonar-ldap-plugin' do
+    version '1.4'
+end
+
+template 'sonar.properties.ldap' do
+   path "#{node[:sonar][:dir]}/conf/sonar.properties.ldap"
+   source '/sonar/sonar.properties.ldap.erb'
+   owner 'root'
+   group 'root'
+   mode 0644
+end
+
+execute "Add LDAP properties to sonar.properties file" do
+  user "root"
+  command "cat #{node[:sonar][:dir]}/conf/sonar.properties.ldap >> #{node[:sonar][:dir]}/conf/sonar.properties"
+  notifies :restart, 'service[sonar]'
+end
