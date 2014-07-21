169153ed001f141e914e9505c779c8024f6138db
Nikolay Yurin <nyurin@griddynamics.com>
2014-07-21 18:56:37 +0400

    [DMG-353] Add LDAP setup

--- cookbooks/cicd_infrastructure/attributes/gerrit.rb
+++ cookbooks/cicd_infrastructure/attributes/gerrit.rb
@@ -7,5 +7,25 @@
 #
 
 default['gerrit']['version'] = "2.8.6"
+
+if node['cloud']
+  case node['cloud']['provider']
+  when "ec2"
+    default['gerrit']['hostname'] = node['cloud']['public_hostname']
+  when "rackspace"
+    default['gerrit']['hostname'] = node['cloud']['private_hostname']
+  end
+end
+
+default['gerrit']['canonicalWebUrl'] = "http://#{node['gerrit']['hostname']}/"
+
 default['gerrit']['auth']['registerEmailPrivateKey'] = 'gerrit'
 default['gerrit']['auth']['restTokenPrivateKey'] = 'gerrit'
+default['gerrit']['auth']['type'] = 'LDAP'
+
+default['gerrit']['ldap'] = {
+  'server' => 'ldap://localhost',
+  'accountBase' => 'ou=people,dc=example,dc=com',
+  'accountPattern' => '(&(objectClass=inetOrgPerson)(uid=${username}))',
+  'accountFullName'=> 'displayName'
+}
