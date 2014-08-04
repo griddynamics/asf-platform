08b999cd71874e386985bf5b20d5f51224c2cdf9
Grigory Silantiev <gsilantyev@griddynamics.com>
2014-08-04 17:37:38 +0400

    [DMG-352] Fix recipe name

--- cookbooks/cicd_infrastructure/recipes/integration_jira_ldap.rb
+++ cookbooks/cicd_infrastructure/recipes/integration_jira_ldap.rb
@@ -1,7 +1,7 @@
 # Encoding: utf-8
 #
 # Cookbook Name:: cicd_infrastructure
-# Recipe:: jira
+# Recipe:: integration_jira_ldap
 #
 # Copyright (c) 2014 Grid Dynamics International, Inc. All Rights Reserved
 # Classification level: Public
