2ed4740a42388ece434ea0b5e5571e1eb78d28e5
Grigory Silantiev <gsilantyev@griddynamics.com>
2014-09-02 18:59:17 +0400

    [DMG-397] Move configuration from settings.xml to maven-global-settings.xml

--- cookbooks/cicd_infrastructure/recipes/integration_jenkins_nexus.rb
+++ cookbooks/cicd_infrastructure/recipes/integration_jenkins_nexus.rb
@@ -27,27 +27,6 @@ end
 
 nexus_config = node['cicd_infrastructure']['nexus']
 
-template "#{node['jenkins']['master']['home']}/.m2/settings.xml" do
-  source 'jenkins/settings.xml.erb'
-  owner node['jenkins']['master']['user']
-  group node['jenkins']['master']['group']
-  mode 0644
-  variables(
-    nexus_username:			node['cicd_infrastructure']['nexus']['login'],
-    nexus_password:			node['cicd_infrastructure']['nexus']['password'],
-    nexus_url:				node['cicd_infrastructure']['jenkins']['nexus']['endpoint'],
-    settings_id:			node['cicd_infrastructure']['jenkins']['cfg_provider']['settings_id'],
-    cfg_plugin_version:		node['cicd_infrastructure']['jenkins']['plugins']['config-file-provider'],
-    jbehave_framework_repo_id:		nexus_config['repo']['jbehave']['id'],
-    jbehave_framework_repo_name:	nexus_config['repo']['jbehave']['name'],
-    jbehave_framework_repo_policy:	nexus_config['repo']['jbehave']['policy'],
-    jbehave_framework_repo_location:	nexus_config['repo']['jbehave']['location'],
-    jbehave_framework_repo_username:	nexus_config['repo']['jbehave']['username'],
-    jbehave_framework_repo_password:	nexus_config['repo']['jbehave']['password'],
-    use_jbehave_proxy:			nexus_config['repo']['jbehave']['use']
-  )
-end
-
 template "#{node['jenkins']['master']['home']}/maven-global-settings-files.xml" do
   source 'jenkins/maven-global-settings-files.xml.erb'
   owner node['jenkins']['master']['user']
@@ -58,7 +37,10 @@ template "#{node['jenkins']['master']['home']}/maven-global-settings-files.xml" 
     nexus_password: node['cicd_infrastructure']['nexus']['password'],
     nexus_url: node['cicd_infrastructure']['jenkins']['nexus']['endpoint'],
     settings_id: node['cicd_infrastructure']['jenkins']['cfg_provider']['settings_id'],
-    cfg_plugin_version: node['cicd_infrastructure']['jenkins']['plugins']['config-file-provider']
+    cfg_plugin_version: node['cicd_infrastructure']['jenkins']['plugins']['config-file-provider'],
+    jbehave_framework_repo_id:         nexus_config['repo']['jbehave']['id'],
+    jbehave_framework_repo_name:       nexus_config['repo']['jbehave']['name'],
+    use_jbehave_proxy:                 nexus_config['repo']['jbehave']['use']
   )
   notifies :restart, 'service[jenkins]'
 end
