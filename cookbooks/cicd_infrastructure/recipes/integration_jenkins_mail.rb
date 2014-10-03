00a4d1868afaa0168b7b12debc4adb89e9436b80
Nickolay Yurin <yurinnick93@gmail.com>
2014-10-03 12:08:21 +0000

    [DMG-376] Add jenkins build email notifications

---
+++ cookbooks/cicd_infrastructure/recipes/integration_jenkins_mail.rb
@@ -0,0 +1,56 @@
+# Encoding: utf-8
+#
+# Cookbook Name:: cicd-infrastructure
+# Attributes:: integration_jenkins_mail
+#
+# Copyright (c) 2014 Grid Dynamics International, Inc. All Rights Reserved
+# Classification level: Public
+# Licensed under the Apache License, Version 2.0.
+#
+
+include_recipe 'cicd_infrastructure::postfix'
+
+service 'jenkins' do
+  action :nothing
+end
+
+mail_config = node['cicd_infrastructure']['jenkins']['mail']
+jenkins = node['jenkins']['master']
+
+template "#{jenkins['home']}/hudson.tasks.Mailer.xml" do
+  source 'integration/jenkins/hudson.tasks.Mailer.xml.erb'
+  owner jenkins['user']
+  group jenkins['group']
+  mode 0644
+  variables(
+    plugin_version: node['cicd_infrastructure']['jenkins']['plugins']['mailer'],
+    admin_email: mail_config['adress'],
+    smtp_host: mail_config['smtp']['host']
+  )
+  notifies :restart, 'service[jenkins]'
+end
+
+template "#{jenkins['home']}/hudson.plugins.emailext.ExtendedEmailPublisher.xml" do
+  source 'integration/jenkins/hudson.plugins.emailext.ExtendedEmailPublisher.xml.erb'
+  owner jenkins['user']
+  group jenkins['group']
+  mode 0644
+  variables(
+    plugin_version: node['cicd_infrastructure']['jenkins']['plugins']['email-ext'],
+    smtp_host: mail_config['smtp']['host']
+  )
+  notifies :restart, 'service[jenkins]'
+end
+
+template "#{jenkins['home']}/jenkins.model.JenkinsLocationConfiguration.xml" do
+  source 'integration/jenkins/jenkins.model.JenkinsLocationConfiguration.xml.erb'
+  owner jenkins['user']
+  group jenkins['group']
+  mode 0644
+  variables(
+    jenkins_email: mail_config['adress'],
+    jenkins_host: jenkins['host'],
+    jenkins_port: jenkins['port']
+  )
+  notifies :restart, 'service[jenkins]'
+end
