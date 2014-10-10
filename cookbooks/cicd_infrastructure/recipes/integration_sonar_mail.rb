5db42ce6407fb74dd4e6f3c9b469e119b4313de8
Nikolay Yurin <nyurin@griddynamics.com>
2014-10-10 15:49:38 +0400

    [DMG-376] Add Sonar email notifications setup

---
+++ cookbooks/cicd_infrastructure/recipes/integration_sonar_mail.rb
@@ -0,0 +1,27 @@
+# Encoding: utf-8
+#
+# Cookbook Name:: cicd_infrastructure
+# Recipe:: integration_sonar_mail
+# Copyright (c) 2014 Grid Dynamics International, Inc. All Rights Reserved
+# Classification level: Public
+# Licensed under the Apache License, Version 2.0.
+#
+
+service 'sonar' do
+  action :nothing
+end
+
+sonar_credentials = node['cicd_infrastructure']['sonar']['credentials']
+
+node['cicd_infrastructure']['sonar']['mail'].each do |key, value|
+  ruby_block "set sonar property #{key}:#{value}" do
+    block do
+      uri = URI.parse("http://localhost:9000")
+      http = Net::HTTP.new(uri.host, uri.port)
+      request = Net::HTTP::Post.new("/api/properties?id=email.#{key}&value=#{value}")
+      request.basic_auth sonar_credentials['username'], sonar_credentials['password']
+      response = http.request(request)
+    end
+    notifies :restart, "service[sonar]"
+  end
+end
